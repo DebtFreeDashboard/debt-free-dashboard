341cb8a51aaed3aa3ec73caae4850c0e70824b26
DebtFreeDashboard <kevin.sawler@gmail.com>
2026-08-31 14:36:34 -0400

    lump sum optimizer fix

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // release so installed PWA clients always re-fetch, and so you can tell at a
 // glance which build a device has cached.
 
-const CACHE_NAME = 'debtfree-1.36.1';
+const CACHE_NAME = 'debtfree-1.36.2';
 const CORE_ASSETS = [
   './',
   './dashboard.html',
