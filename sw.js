6e5caa805ef071c45d5dbec8cb6b141cad9017ab
DebtFreeDashboard <kevin.sawler@gmail.com>
2026-09-02 14:30:48 -0400

    update

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // release so installed PWA clients always re-fetch, and so you can tell at a
 // glance which build a device has cached.
 
-const CACHE_NAME = 'debtfree-1.38.1';
+const CACHE_NAME = 'debtfree-1.39.0';
 const CORE_ASSETS = [
   './',
   './dashboard.html',
